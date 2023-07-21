6094f70daa4656116afc9d4fe2b105edc75236e3
eyupkarakas <113632376+eyupkarakas@users.noreply.github.com>
2023-07-21 12:21:33 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        double kdv =0.18;
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("KDV'siz fiyatı giriniz : ");
+        int a= input.nextInt();
+        if(a>1000){
+            kdv=0.08;
+        }
+         double kdvli = a*kdv+a;
+         double kdvTutarı =a*kdv;
+         System.out.println("KDV'li fiyat : "+kdvli);
+         System.out.println("KDV tutarı :" +kdvTutarı);
+         
         }
     }
