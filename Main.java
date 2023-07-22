610a2265bc0720cc0d60e9dfce1cea47b8bc7ed6
eyupkarakas <113632376+eyupkarakas@users.noreply.github.com>
2023-07-22 12:06:11 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,37 @@
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
+        public static void main(String[] args){
+            int sayac = 0;
+            for(int sayi=2;sayi<=100;sayi++)
+            {
+                int kontrol = 0;
+                for (int i = 2; i < sayi; i++)
+                {
+                    if (sayi % i == 0)
+                    {
+                        kontrol = 1;
+                        break;
+                    }
+                }
+
+                if(kontrol==0)
+                {
+                    System.out.print(sayi+"\n");
+                    sayac++;
+                }
+            }
+        }
+}
+
+    /*public static void main(String[] args) {
+        System.out.print("1.dik kenarı gir");
+        System.out.print("2.dik kenarı gir");
+        System.out.print(" kenarı gir");
+        DikUcgen dikUcgen = new DikUcgen();
+
+
+
+
         double kdv =0.18;
 
         Scanner input = new Scanner(System.in);
@@ -9,10 +40,9 @@ public class Main {
         if(a>1000){
             kdv=0.08;
         }
-         double kdvli = a*kdv+a;
-         double kdvTutarı =a*kdv;
-         System.out.println("KDV'li fiyat : "+kdvli);
-         System.out.println("KDV tutarı :" +kdvTutarı);
-         
-        }
-    }
+        double kdvli = a*kdv+a;
+        double kdvTutarı =a*kdv;
+        System.out.println("KDV'li fiyat : "+kdvli);
+        System.out.println("KDV tutarı :" +kdvTutarı);
+
+    }*/
